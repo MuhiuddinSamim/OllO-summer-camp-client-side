b7167e73b4328f337694e412970313b880cce150
samim hossain sujon <samimhossainsanto@gmail.com>
2023-06-11 16:01:22 +0600

    dark mode and light mode added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+  daisyui: {
+    themes: [
+      "light",
+      "dark",      
+    ],
+  },
 
+};
